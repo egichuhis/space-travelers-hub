7d86d08667831ed637981908a58e42027174c55a
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-10-02 14:46:31 +0300

    Add navbar and routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import LogoImg from '../imgs/planet.png';
+
+const NavBar = () => (
+  <nav
+    className="navbar navbar-expand-md bg-body py-3"
+    style={{ paddingTop: 16, paddingBottom: 16, boxShadow: '1px 1px' }}
+  >
+    <div className="container">
+      <a className="navbar-brand d-flex align-items-center" href="/">
+        <span
+          className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"
+          style={{ background: 'white' }}
+        >
+          <img
+            src={LogoImg}
+            alt="logo"
+            width={37}
+            height={71}
+            style={{ height: 50, width: 50 }}
+          />
+        </span>
+        <span style={{ marginLeft: 10 }}>Space Travelers Hub</span>
+      </a>
+      <button
+        type="button"
+        className="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navcol-2"
+      >
+        <span className="visually-hidden">Toggle navigation</span>
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div id="navcol-2" className="collapse navbar-collapse">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <a className="nav-link active" href="/">
+              Rockets
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="/missions"
+              style={{ borderRightWidth: 1, borderRightStyle: 'solid' }}
+            >
+              Missions
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="profile">
+              My Profile
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+);
+
+export default NavBar;
---
+++ src/components/Rockets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Rockets = () => (
+  <>
+    <NavBar />
+  </>
+);
+
+export default Rockets;
