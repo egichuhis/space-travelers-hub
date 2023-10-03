5402b172efe16d9c35810bece1132f59421faafd
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-10-03 15:47:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Rockets />} />
-        <Route path="/" element={<Profile />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
