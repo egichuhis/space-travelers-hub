120925e3b574adcaaa333fbb8138899220b2c8ab
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-10-03 11:28:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Rockets from './components/Rockets/Rockets';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Rockets />} />
+        <Route path="/" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
